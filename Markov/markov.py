d1601f830b30d37165b240f2c438bc6627bbc86d
ladezai <------>
2021-08-08 19:29:40 +0200

    Improved documentation. Fixed an error in evaluate_next.

--- Markov/markov.py
+++ Markov/markov.py
@@ -1,30 +1,121 @@
 import numpy as np
-from numpy.random import choice
 from math import isclose 
 
-class MarkovModel():
+class StationaryMarkovChain():
     """
-        TODO: add documentation
+        It provides an implementation of stationary finite
+        Markov chain `(l, P)` over the set of labels `S` 
+        (or nodes of the Markov chain). 
+
+        The following implementation emphasises that a Markov 
+        chain represents the evolution of a distribution over 
+        time, via a stocastic process determined by `P`. 
+        Therefore from an iterable point of view the class is 
+        only its distribution, i.e. of type``dict[str,float]``.
+        And ``__next__`` method is replaced by ``evaluate_next``. 
+        In such a way that allows to easily simulate a path over 
+        itself via for-loops.
+        
+        See ``../examples/word_generator.py`` or 
+        ``../examples/wordplay_test.py``.
+
+        Example
+        -------
+        We represent a trivial markov chain via the following graph:
+
+         ._0.5_             .__1__
+        |     |            |     |
+        '--> a ----0.5---> b <--'
+
+        then the set S = {'a', 'b'} and the stocastic matrix P
+        is the following 
+        P = [[0.5,0.5],[0, 1]]
+
+        In terms of the implementation below the set S
+        represents the keys of the initial distribution, while
+        P is the iteration_matrix. Since no initial distribution
+        is given in our example we could consider the Dirac 
+        distribution which is 1 in 'a' and 0 in 'b', therefore
+        initial_distribution = {'a' : 1.0, 'b': 0.0}.
+
+
+        Attributes
+        ----------
+        iteration_matrix : numpy.array
+            A stocastic matrix which represents links 
+            between each node in the Markov Chain's state.
+        current_distr    : dict[str,float]
+            A dictionary that provides an initial distribution
+            for the MarkovChain. Where keys are used
+            as label for each node of the Markov Chain's state.
+
+        Methods
+        --------
+        set_distr : dict[str,float] -> bool -> None
+            Assigns a new value to current_distribution. 
+            Checks whether the new value is also a distribution.
+        reset_distr : None
     """
+
+
     iteration_matrix = np.array([[]])
     current_distr    = dict()
     
-    def __init__(self, iteration_matrix:np.array, distribution:dict[str, float]):
+    def __init__(self, iteration_matrix:np.array, initial_distribution:dict[str, float]):
         """
-            TODO
+            Parameters
+            ----------
+            iteration_matrix : numpy.array
+                The Markov Chain's stocastic matrix, it has to be a 
+                square matrix with normalized rows.  
+            initial_distribution : dict[str, float]
+                A dictionary which provides an initial distribution.
+                Note that it must have same length as the rows/columns 
+                of the iteration_matrix.
+
+            Raises
+            ------
+            ValueError
+                If initial_distribution and iterations_matrix aren't 
+                of same length.
         """
+        if iteration_matrix.shape[0] != iteration_matrix.shape[1]:
+            raise ValueError("Iteration matrix must be a square-matrix")
+
+        if len(initial_distribution) != iteration_matrix.shape[0]:
+            raise ValueError(("Initial distribution and iteration matrix does" +
+                             "not have same size."))
+        
         self.iteration_matrix = iteration_matrix
-        self.current_distr    = distribution
+        self.current_distr    = initial_distribution
+        
 
 
     ###########################################################################
     ################## Get, set and update distribution #######################
     ###########################################################################
 
-    def set_current_distr(self, distr:dict[str,float], checks : bool = False):
+    def set_distr(self, distr:dict[str,float], checks : bool = False):
         """
-            TODO
+            Parameters
+            ----------
+            distr : dict[str, float]
+                A dictionary which provides a distribution.
+                Note that it must have same length as the rows/columns 
+                of the iteration_matrix.
+            checks : bool 
+                It provides a way to check whether the distr parameter 
+                is a distribution (default is False).
+
+            Raises
+            ------
+            ValueError
+                If initial_distribution and iterations_matrix aren't 
+                of same length.
+            ValueError 
+                If distr is not normalized.
         """
+
         if checks:
             M = sum(list(distr.values()))
             if not isclose(M,1,abs_tol=0.0001):
@@ -41,14 +132,6 @@ class MarkovModel():
             TODO
         """
         self.current_distr = {k:0 for k in self.current_distr}
-        return None
-
-    def update_distr(self, node:str, value:float) -> None:
-        """
-            TODO
-        """
-        self.current_distr[node] = value
-        
         return None
 
     def set_to_dirac_distr(self, node:str) -> None:
@@ -72,9 +155,10 @@ class MarkovModel():
         """
             Evaluate in-place the new distribution after 1 step.
         """
+
         for i in range(n):
             values      = np.array(list(self.current_distr.values()))
-            new_values  = self.iteration_matrix.dot(values) 
+            new_values  = self.iteration_matrix.T.dot(values) 
             
             i = 0
             for key in self.current_distr:
