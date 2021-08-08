3ca2a6e8b6bb96330b747596da3c2da15263bf98
ladezai <------>
2021-08-08 14:34:06 +0200

    now uses dictionaries as distributions, and it's iterable.

--- Markov/markov.py
+++ Markov/markov.py
@@ -1,131 +1,76 @@
 import numpy as np
 from numpy.random import choice
+from math import isclose 
 
 class MarkovModel():
     """
-        A Markov model is a data structure composed of 3 elements:
-            1. `MarkovModel.iteration_matrix` which is a 2D numpy array 
-                of shape `n x n` (i.e. matrix) iteration matrix of the 
-                discrete markov model. 
-            2. `MarkovModel.nodes_label` is an array to substitute 
-                each of the `n` nodes with a label. 
-            3. `MarkovModel.current_state` which represents the point
-                at which the simulation is at.
-        
-        In order to generate a Markov Model one has to call a 
-        constructor method, which gives an `iteration_matrix` 
-        (a n x n numpy array), `nodes_label` has to have length 
-        `n` and is facoltative,
-        `initial_state` is also facoltative and is an integer between
-        `0` and `n-1`.     
+        TODO: add documentation
     """
     iteration_matrix = np.array([[]])
-    nodes_label      = np.array([])
-    rows             = 0
-    current_state    = 0
+    current_distr    = dict()
     
-    
-    def __init__(self, iteration_matrix, nodes_label=None, initial_state=0):
+    def __init__(self, iteration_matrix:np.array, distribution:dict[str, float]):
+        """
+            distribution : Dict  
+        """
         self.iteration_matrix = iteration_matrix
-        self.nodes_label      = nodes_label
-        self.current_state    = initial_state
-        self.rows             = self.iteration_matrix.shape[0]
-    
+        self.current_distr    = distribution
 
-    def set_current_state(self, state):
+    def set_current_distr(self, distr:dict[str,float], checks=False):
         """
-            Set current `state` based on nodes' labels.
-
-            Example:
-            ```
-                > mm = MarkovModel([[1,0][0,1]], nodes_label=["pizza","soup"])
-                > mm.set_current_state("pizza")
-                > mm.current_state
-                  0
+            
         """
-        if self.nodes_label.shape[0] < 1:
-            raise NameError("There are no labels saved in the Markov Model")
-            return
+        if checks:
+            M = sum(list(distr.values()))
+            if not isclose(M,1,abs_tol=0.0001):
+                raise ValueError("Dictionary given is not a distribution.")
 
-        if not state in self.nodes_label:
-            raise NameError("The state isn't in MarkovModel.nodes_label")
-            return
+            if len(self) != len(distr):
+                raise ValueError("Distributions have different length.")
 
-        self.current_state = np.nonzero(self.nodes_label == state)[0][0]
-        return self.current_state
+        self.current_distr = distr
+        return None
 
-     
-    def evaluate(iteration_matrix, choices, initial_state):
-        """
-            A static version of `evaluate_next`.
-        """
-        return choice(choices, 1, p=iteration_matrix[initial_state])[0]
+    def reset_distr(self):
+        ""
+        self.current_distr = {k:0 for k in self.current_distr}
+        return None
 
-
-    def evaluate_next(self):
-        """
-            Assume a Markov model that has probability $1$
-            that is in $u_0 \in \{0, \dots, n-1\}$.
-            And further assume an iteration matrix $A$ with 
-            dimensions $n \times n$. 
-            It evaluates, based on the distribution given 
-            by $M \cdot x_0$, which of the $\{0, \dots, n-1\}$
-            state should follow. 
-        """
-        self.current_state = MarkovModel.evaluate(self.iteration_matrix, 
-                            self.rows,
-                            self.current_state)
-        return self.current_state
-   
-
-    def evaluate_after_n(self,n):
-        """1
-            Basically the same as `evaluate_next` although 
-            the iteration matrix is given by `A^n`.
-        """
-        self.current_state = MarkovModel.evaluate(self.iteration_matrix**n, 
-                                        self.rows, 
-                                        self.current_state)
-        return self.current_state
-
-
-    def evaluate_path_of_n_steps(self,n):
-        """
-            Evaluates a path of n steps on the Markov Model and 
-            returns an array of the nodes passed bys.
+    def update_distr(self, node:str, value:float):
         """
         
-        iters = np.zeros(n)
-        for i in range(n):
-            iters[i] = self.evaluate_next()
+        """
+        self.current_distr[node] = value
         
-        return iters
+        return None
 
-    def state_to_label(self,state):
+    def set_to_dirac_distr(self, node:str):
+        self.reset_distr()
+        self.current_distr[node] = 1
+
+        return None
+
+    def evaluate_next(self, n=1, normalize=True):
         """
-            Requires an integer `state' between `0` and 
-            `MarkovModel.nodes_label`'s  length. 
+            Evaluate in-place the new distribution after 1 step.
         """
+        while n >= 1:
+            values = np.array(list(self.current_distr.values()))
+            new_values  = self.iteration_matrix.dot(values) 
+            
+            i = 0
+            for key in self.current_distr:
+                self.current_distr[key] = new_values[i]
+                i += 1 
+            
+            del i
 
-        # Since numpy stores efficiently floating point
-        # and most times I just use numpy's arrays, 
-        # I have to round the floating points. 
-        state = round(state)
+            # Normalization to a distribution
+            if normalize:
+                self.normalize_distr()
 
-        if state > self.rows or state < 0: 
-            raise NameError("state given is higher the maximum  \
-                            value or lower than minimum value")
-            return 
-
-        return self.nodes_label[state]
-
-    def path_to_label_path(self,path):
-        """
-            Given an array of integers, `0` and 
-            `self.nodes_label`'s  length,  
-            returns an array of `self.nodes_labels`. 
-        """
-        return np.array(list(map(self.state_to_label,path)))
+            n -= 1
+        return None
 
     def normalize(self):
         """
@@ -135,10 +80,37 @@ class MarkovModel():
             Note: It's damaging repeating this function 
             over-and-over again. 
         """
-        for i in range(self.rows):
+        l = len(self)
+        for i in range(l):
             N = sum(self.iteration_matrix[i])
             self.iteration_matrix[i] /= N
+        return None
+
+    def normalize_distr(self):
+        K = sum(list(self.current_distr.values()))
+        for key in self.current_distr:
+            self.current_distr[key] /= K
+
+        return None
+    
+    def distribution(self):
+        return self.current_distr
+    
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        self.evaluate_next(n=1,normalize=True)
+        return self.current_distr
+
+    def __del__(self):
+        del self.current_distr
+        del self.iteration_matrix
+
+    def __len__(self):
+        return len(self.current_distr)
+
+    def __str__(self):
+        return str(self.iteration_matrix) + str(self.current_distr)
 
     
-    def __str__(self):
-        return str(self.iteration_matrix) + str(self.nodes_label)
